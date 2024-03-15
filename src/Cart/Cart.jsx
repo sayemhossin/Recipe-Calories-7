e9f5a8f130797d9d28451f05b6e7ceb896e6d7b7
sayemhossin <sayemhossin22@gmail.com>
2024-03-16 03:55:09 +0600

    added more logic

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -18,8 +18,8 @@ const Cart = ({cart,handleDelete,handleAdd,cook}) => {
                 <div key={index} className="flex text-[14px] md:text-[18px]  bg-gray-200  p-5  justify-between text-[#878787]">
                     <p className="flex items-center font-semibold text-black">{index+1}</p>
                      <p className="w-8">{item.recipe_name}</p>
-                <p className="w-8">{item.preparing_time}</p>
-                <p className="w-8">{item.calories}</p>
+                <p className="w-8">{item.preparing_time} min</p>
+                <p className="w-8">{item.calories} calories</p>
                 <button onClick={()=>{handleDelete(item); handleAdd(item);}} className="rounded-lg h-full bg-[#0BE58A] hover:bg-[#32bb82] border-0 text-black p-1 md:p-2 ">Preparing</button>
                 </div>
             ))
@@ -27,7 +27,7 @@ const Cart = ({cart,handleDelete,handleAdd,cook}) => {
             </div>
             <h1 className="text-center  text-3xl text-[#282828] font-semibold mt-20">Currently cooking: {cook.length}</h1>
             <div className="divider"></div>
-            <div className="flex justify-around text-[#878787]">
+            <div className="flex gap-16 ml-28 md:justify-around text-[#878787]">
                 <p>Name</p>
                 <p>Time</p>
                 <p>Calories</p>
@@ -38,8 +38,8 @@ const Cart = ({cart,handleDelete,handleAdd,cook}) => {
                         <div key={index} className="flex   bg-gray-200  p-5  justify-around text-[#878787]">
                             <p className="flex items-center font-semibold text-black">{index+1}</p>
                              <p className="w-8">{item.recipe_name}</p>
-                        <p className="w-8">{item.preparing_time}</p>
-                        <p className="w-8">{item.calories}</p>
+                        <p className="w-8">{item.preparing_time} min</p>
+                        <p className="w-8">{item.calories} calories</p>
                        
                         </div>
 
@@ -51,8 +51,10 @@ const Cart = ({cart,handleDelete,handleAdd,cook}) => {
                 <h1>Total Calories =</h1>
             </div>
             <div className="flex justify-end gap-20 mb-10 mr-7 text-[#878787]">
-                <h1>{cook.map(c => c.preparing_time)}</h1>
-                <h1>{cook.map(c => c.calories)}</h1>
+              
+               <h1>{cook.reduce((p,a) => p+ a.preparing_time,0)} min</h1>
+                <h1>{cook.reduce((p,a) => p+ a.calories,0)} calories</h1>
+              
             </div>
 
         </div>
